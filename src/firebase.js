a4d5a3d8b8c088ec8d02476901c35316893cebf8
bosswrk <warakorn.sirinantikul@gmail.com>
2019-09-12 11:40:16 +0700

    remove firebase key

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: 'AIzaSyCjRLSMHZvuds7HDi992LunA8GoVacRq-0',
-    authDomain: 'bosswrk-pg-real-time-chat.firebaseapp.com',
-    databaseURL: 'https://bosswrk-pg-real-time-chat.firebaseio.com',
-    projectId: 'bosswrk-pg-real-time-chat',
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
     storageBucket: '',
-    messagingSenderId: '808388647533',
+    messagingSenderId: '',
 };
 
 firebase.initializeApp(config);
